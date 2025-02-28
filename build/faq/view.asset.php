ffa397265b538f959233af1894720971b69bd117
sagarshrestha <rarasagar321@gmail.com>
2025-02-28 15:21:02 +0545

    Added the gitignore

--- build/faq/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '8371cf5c87898374dc4f');
--- build/slider/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '1fb7390180a0a1fd1594');
